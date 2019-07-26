608c40846cade14e6cf713e50a503340496d3eca
Tasewell Fox <tasewell.j.fox@accenture.com>
2019-07-26 13:24:46 -0500

    Start ripping apart the aged brie and ticket behavior. Get tests around brie behavior.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -10,30 +11,49 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    item.quality = item.quality - 1
+            if item.name == "Aged Brie":
+                item.sell_in = item.sell_in - 1
+                self.update_appreciating_item(item)
+
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.update_time_sensitive_item(item)
+                self.update_item_sell_in(item)
+
             else:
+                self.update_item_quality(item)
+                self.update_item_sell_in(item)
+
+    def update_item_quality(self, item):
+        if item.quality > 0:
+            item.quality = item.quality - 1
+
+    def update_time_sensitive_item(self, item):
+            item.quality = min(50, item.quality + 1)
+            if item.sell_in < 11:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+
+    def update_item_sell_in(self, item):
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            self.update_expired_item(item)
+
+    def update_expired_item(self, item):
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            item.quality = item.quality - item.quality
+        elif item.name == "Aged Brie":
+            item.quality = min(50, item.quality + 1)
+        else:
+            item.quality = max(0, item.quality - 1)
+
+    def update_appreciating_item(self, item):
+        if item.sell_in < 0:
+            item.quality += 2
+        else:
+            item.quality += 1
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -8,6 +8,7 @@ from legacyMasterData import legacy_output, legacy_test_set, legacy_test_duratio
 
 class GildedRoseTest(unittest.TestCase):
     def test_legacy_system(self):
+        self.maxDiff = None
         items = copy.deepcopy(legacy_test_set)
         output = ''
         for day in range(legacy_test_duration):
@@ -27,6 +28,18 @@ class GildedRoseTest(unittest.TestCase):
 
         self.assertEqual(items[0].quality, 65)
         self.assertEqual(items[0].sell_in, 0)
+
+    def test_update_appreciating_item__should_increase_quality_by_one_if_not_expired(self):
+        items = [Item("Aged Brie", 4, 0)]
+        GildedRose(items).update_quality()
+        print(items)
+        self.assertEqual(1, items[0].quality)
+
+    def test_update_appreciating_item__should_increase_quality_by_two_if_expired(self):
+        items = [Item("Aged Brie", -1, 0)]
+        GildedRose(items).update_quality()
+
+        self.assertEqual(items[0].quality, 2)
 
 
 if __name__ == '__main__':
